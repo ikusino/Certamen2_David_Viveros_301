5e33c2dd0f6a7674ba8a5d1256d3bfd83e445ea1
ikusino <103014548+ikusino@users.noreply.github.com>
2022-11-18 00:53:14 -0300

    Add files via upload

---
+++ dam_c2_api/app/Http/Controllers/AutosController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Autos;
+use Illuminate\Http\Request;
+
+class AutosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Autos  $autos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Autos $autos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Autos  $autos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Autos $autos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Autos  $autos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Autos $autos)
+    {
+        //
+    }
+}
